064cec04d47a0c64eb5a352ffa6b488f3287eec0
prasetyama <prasetyama@gmail.com>
2025-11-17 11:16:53 +0700

    update icon and form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { EyeSlashIconComponent } from './shared/components/icons/icon-eye-slash';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, EyeSlashIconComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
@@ -22,6 +23,25 @@ export class AppComponent {
   onSubmit(): void {
     if (this.signUpForm.valid) {
       console.log(this.signUpForm.getRawValue());
+    }
+  }
+
+  toggleConfirmPasswordVisibility(): void {
+    console.log('Toggling confirm password visibility');
+    const input = document.getElementById('confirmPasswordInput') as HTMLInputElement;
+    if (input.type === 'password') {
+      input.type = 'text';
+    } else {
+      input.type = 'password';
+    }
+  }
+
+  togglePasswordVisibility(): void {
+    const input = document.getElementById('passwordInput') as HTMLInputElement;
+    if (input.type === 'password') {
+      input.type = 'text';
+    } else {
+      input.type = 'password';
     }
   }
 }
---
+++ src/app/shared/components/icons/icon-eye-slash.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'icon-eye-slash',
+  standalone: true,
+  template: `
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10 13.5371C11.6569 13.5371 13 12.194 13 10.5371C13 8.88026 11.6569 7.53711 10 7.53711C8.34315 7.53711 7 8.88026 7 10.5371C7 12.194 8.34315 13.5371 10 13.5371Z" stroke="#87858B" stroke-width="1.2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M10.5 15.5371C15.1944 15.5371 19 13.2985 19 10.5371C19 7.77569 15.1944 5.53711 10.5 5.53711C5.80558 5.53711 2 7.77569 2 10.5371C2 13.2985 5.80558 15.5371 10.5 15.5371Z" stroke="#87858B" stroke-width="1.2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M4 3.53711L17 16.5371" stroke="white" stroke-width="2.5" stroke-linecap="round"/>
+        <path d="M4 3.53711L17 16.5371" stroke="#87858B" stroke-width="1.2" stroke-linecap="round"/>
+    </svg>
+
+  `
+})
+export class EyeSlashIconComponent {}
