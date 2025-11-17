a89593bd3cc2916d9b580918d3bf3d2cd3aa305a
prasetyama <prasetyama@gmail.com>
2025-11-17 14:36:19 +0700

    update set show and hide password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { EyeSlashIconComponent } from './shared/components/icons/icon-eye-slash';
+import { EyeIconComponent } from './shared/components/icons/icon-eye';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, EyeSlashIconComponent],
+  imports: [CommonModule, ReactiveFormsModule, EyeSlashIconComponent, EyeIconComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
@@ -18,6 +19,9 @@ export class AppComponent {
     acceptTerms: [false]
   });
 
+  isPasswordVisible: boolean = false;
+  isConfirmPasswordVisible: boolean = false;
+
   constructor(private readonly fb: FormBuilder) {}
 
   onSubmit(): void {
@@ -26,22 +30,15 @@ export class AppComponent {
     }
   }
 
-  toggleConfirmPasswordVisibility(): void {
-    console.log('Toggling confirm password visibility');
+  toggleConfirmPasswordVisibility() {
     const input = document.getElementById('confirmPasswordInput') as HTMLInputElement;
-    if (input.type === 'password') {
-      input.type = 'text';
-    } else {
-      input.type = 'password';
-    }
+    this.isConfirmPasswordVisible = !this.isConfirmPasswordVisible;
+    input.type = this.isConfirmPasswordVisible ? 'text' : 'password';
   }
 
-  togglePasswordVisibility(): void {
+  togglePasswordVisibility() {
     const input = document.getElementById('passwordInput') as HTMLInputElement;
-    if (input.type === 'password') {
-      input.type = 'text';
-    } else {
-      input.type = 'password';
-    }
+    this.isPasswordVisible = !this.isPasswordVisible;
+    input.type = this.isPasswordVisible ? 'text' : 'password';
   }
 }
