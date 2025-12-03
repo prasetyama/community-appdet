49291841ee53d9ea99a75568e8fde22f948d86dc
prasetyama <prasetyama@gmail.com>
2025-12-03 14:26:47 +0700

    takeout google credential

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,6 +10,6 @@ export const environment = {
       appId: "1:211455519688:web:f8eff943f252f160506efd",
       measurementId: 'G-2NVVK16ZJ0',
     },
-    google_client_id: '613330545715-j1o12cqbpu5qio9b8v3rdc2g6aa3d5r3.apps.googleusercontent.com',
-    google_client_secret: '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',
+    google_client_id: '',
+    google_client_secret: '',
 };
